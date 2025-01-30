35c5cd884fa5bde4c744d9e78e5cf1d57ed54a19
zynoxus <52yusufcr52@gmail.com>
2025-01-30 15:03:20 +0300

    1.gün
    en basit şekilde ağacı baştan oluşturduk.
    Önce ne yapmak istediğimizi belirlemek için 2 adet TextField oluşturdum. Hemen altına 4 adet ElevatedButton kullanarak bu butonlara yapmaları gereken işlemleri atadım. Text field larımı içine yazılanları kontrol edebilmek için iki adet Controler ile yazılanları 2 nesneye aktardım. sonra girilen değerleri tolamak için num değerindeki değişkenlere num değerinde aktardım.
    ve işlemleri tanımladım.

--- basit_hesaplayici_makine/lib/main.dart
+++ basit_hesaplayici_makine/lib/main.dart
@@ -1,125 +1,112 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Hesaplayici());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Hesaplayici extends StatelessWidget {
+  const Hesaplayici({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'hesaplayıcı makine',
+      home: const Hesap(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Hesap extends StatelessWidget {
+  const Hesap({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Basit Hesaplayıcı Makine'),
+      ),
+      body: Anaekran(),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class Anaekran extends StatefulWidget {
+  const Anaekran({super.key});
 
-  void _incrementCounter() {
+  @override
+  State<Anaekran> createState() => _AnaekranState();
+}
+
+class _AnaekranState extends State<Anaekran> {
+  num sayi1 = 1, sayi2 = 2, sonuc = 0;
+  TextEditingController t1 = TextEditingController();
+  TextEditingController t2 = TextEditingController();
+  SayiTopla() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      sayi1 = num.parse(t1.text);
+      sayi2 = num.parse(t2.text);
+
+      sonuc = sayi1 + sayi2;
+    });
+  }
+
+  SayiCikar() {
+    setState(() {
+      sayi1 = num.parse(t1.text);
+      sayi2 = num.parse(t2.text);
+      sonuc = sayi1 - sayi2;
+    });
+  }
+
+  Carp() {
+    setState(() {
+      sayi1 = num.parse(t1.text);
+      sayi1 = num.parse(t2.text);
+      sonuc = sayi1 * sayi2;
+    });
+  }
+
+  Bol() {
+    setState(() {
+      sayi1 = num.parse(t1.text);
+      sayi1 = num.parse(t2.text);
+      sonuc = sayi1 / sayi2;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    return Container(
+      margin: const EdgeInsets.all(20),
+      child: Center(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Text(sonuc.toString()),
+            TextField(
+              controller: t1,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            TextField(
+              controller: t2,
+            ),
+            ElevatedButton(
+              onPressed: SayiTopla,
+              child: Text("topla"),
+            ),
+            ElevatedButton(
+              onPressed: SayiCikar,
+              child: Text("Çıkar"),
+            ),
+            ElevatedButton(
+              onPressed: Carp,
+              child: Text("Çarp"),
+            ),
+            ElevatedButton(
+              onPressed: Bol,
+              child: Text("Böl"),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
